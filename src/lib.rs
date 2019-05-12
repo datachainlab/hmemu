a9eaaba44ee5ea3a8da257ed3bb4a2af058b9fe7
Jun Kimura <jksmphone@gmail.com>
2019-05-12 16:34:39 +0900

    fix functions to return error

--- src/lib.rs
+++ src/lib.rs
@@ -14,24 +14,26 @@ extern "C" {
     fn __switch_process(pid: i32) -> i32;
 }
 
+pub type Result<T> = std::result::Result<T, String>;
+
 thread_local!(static PID: RefCell<i32> = RefCell::new(-1));
 
 // get_mutex gets mutex
-fn get_mutex() {
+fn get_mutex() -> Result<()> {
     unsafe {
         match __get_mutex() {
-            0 => {}
-            _ => panic!("cannot get mutex"),
+            ret if ret < 0 => Err(format!("__get_mutex: error({})", ret)),
+            _ => Ok(()),
         }
     }
 }
 
 // release_mutex release mutex
-fn release_mutex() {
+fn release_mutex() -> Result<()> {
     unsafe {
         match __release_mutex() {
-            0 => {}
-            _ => panic!("cannot release mutex"),
+            ret if ret < 0 => Err(format!("__release_mutex: error({})", ret)),
+            _ => Ok(()),
         }
     }
 }
@@ -43,104 +45,115 @@ fn get_pid() -> i32 {
 
 // This function gets a mutex inside.
 // init_process create a new process.
-pub fn init_process() -> i32 {
+pub fn init_process() -> Result<i32> {
     unsafe {
         match __init_process() {
-            -1 => panic!("abort"),
+            ret if ret < 0 => Err(format!("__init_process: error({})", ret)),
             pid => {
                 PID.with(|p| {
                     *p.borrow_mut() = pid;
                 });
-                pid
+                Ok(pid)
             }
         }
     }
 }
 
-pub fn init_sender(addr: &[u8]) {
+pub fn init_sender(addr: &[u8]) -> Result<()> {
     unsafe {
         match __init_sender(addr.as_ptr(), addr.len()) {
-            -1 => panic!("abort"),
-            _ => {}
+            ret if ret < 0 => Err(format!("__init_sender: error({})", ret)),
+            _ => Ok(())
         }
     }
 }
 
-pub fn init_push_arg<T: Into<String>>(s: T) {
+pub fn init_push_arg<T: Into<String>>(s: T) -> Result<()> {
     let ss = s.into();
     let b = ss.as_bytes();
     unsafe {
         match __init_push_arg(b.as_ptr(), b.len()) {
-            -1 => panic!("abort"),
-            _ => {}
+            ret if ret < 0 => Err(format!("__init_push_arg: error({})", ret)),
+            _ => Ok(())
         }
     }
 }
 
-pub fn init_done() {
+pub fn init_done() -> Result<()> {
     unsafe {
         match __init_done() {
-            -1 => panic!("abort"),
-            _ => {}
+            ret if ret < 0 => Err(format!("__init_done: error({})", ret)),
+            _ => Ok(()),
         }
     }
 }
 
-pub fn commit_state() {
+pub fn commit_state() -> Result<()> {
     unsafe {
         match __commit_state() {
-            -1 => panic!("abort"),
-            _ => {}
+            ret if ret < 0 => Err(format!("__commit_state: error({})", ret)),
+            _ => Ok(()),
         }
     }
 }
 
-pub fn destroy_process() {
+pub fn destroy_process() -> Result<()> {
     unsafe {
         match __destroy_process() {
-            -1 => panic!("abort"),
+            ret if ret < 0 => Err(format!("__destroy_process: error({})", ret)),
             _ => PID.with(|p| {
                 *p.borrow_mut() = -1;
+                Ok(())
             }),
         }
     }
 }
 
-pub fn switch_process(pid: i32) {
+pub fn switch_process(pid: i32) -> Result<()> {
     unsafe {
         match __switch_process(pid) {
-            -1 => panic!("abort"),
+            ret if ret < 0 => Err(format!("__switch_process: error({})", ret)),
             _ => PID.with(|p| {
                 *p.borrow_mut() = pid;
+                Ok(())
             }),
         }
     }
 }
 
-pub fn exec_process<F: FnOnce() -> Result<(), String>>(cb: F) -> Result<(), String> {
+pub fn exec_process<F: FnOnce() -> Result<()>>(cb: F) -> Result<()> {
     exec_process_with_arguments(Vec::<String>::new(), cb)
 }
 
-pub fn exec_process_with_arguments<F: FnOnce() -> Result<(), String>, T: Into<String>>(
+pub fn exec_process_with_arguments<F: FnOnce() -> Result<()>, T: Into<String>>(
     args: Vec<T>,
     cb: F,
-) -> Result<(), String> {
-    get_mutex();
-    init_process();
+) -> Result<()> {
+    get_mutex()?;
+    init_process()?;
     for arg in args.into_iter() {
         let s = arg.into();
-        init_push_arg(s.as_str());
+        init_push_arg(s.as_str())?;
     }
-    init_done();
+    init_done()?;
     let res = cb();
-    destroy_process();
-    release_mutex();
+    destroy_process()?;
+    release_mutex()?;
     res
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
+
+    #[test]
+    fn initialize_test() {
+        init_process().unwrap();
+        init_push_arg("key1").unwrap();
+        init_done().unwrap();
+
+        init_push_arg("key2").expect_err("expect error");
+    }
 
     #[test]
     fn process_test() {
@@ -172,24 +185,24 @@ mod tests {
 
     #[test]
     fn process_manager_test() {
-        get_mutex();
+        get_mutex().unwrap();
 
-        let pid1 = init_process();
+        let pid1 = init_process().unwrap();
         assert_eq!(pid1, get_pid());
-        init_push_arg("key1");
+        init_push_arg("key1").unwrap();
 
-        let pid2 = init_process();
+        let pid2 = init_process().unwrap();
         assert_eq!(pid2, get_pid());
-        init_push_arg("key2");
+        init_push_arg("key2").unwrap();
 
-        switch_process(pid1);
+        switch_process(pid1).unwrap();
         assert_eq!(pid1, get_pid());
         assert_eq!("key1", hmc::get_arg_str(0).unwrap().as_str());
 
-        switch_process(pid2);
+        switch_process(pid2).unwrap();
         assert_eq!(pid2, get_pid());
         assert_eq!("key2", hmc::get_arg_str(0).unwrap().as_str());
 
-        release_mutex();
+        release_mutex().unwrap();
     }
 }
