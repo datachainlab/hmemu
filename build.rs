ebe6b0e2a0cb9de8ddd17e0f08af98a8682628d7
Ryo Sato <3100.dev@gmail.com>
2019-07-08 17:39:41 +0900

    go build in cargo build

--- build.rs
+++ build.rs
@@ -1,4 +1,23 @@
+use std::env;
+use std::path::Path;
+use std::process::Command;
+
 fn main() {
+    // If the package has a build script, this is set to the folder where the build script should place its output.
+    let out_dir = env::var("OUT_DIR").unwrap();
+
+    Command::new("go")
+        .args(&[
+            "build",
+            "-o",
+            &format!("{}/libhm.so", out_dir),
+            "-buildmode=c-shared",
+            "./lib",
+        ])
+        .current_dir(&Path::new("."))
+        .status()
+        .unwrap();
+
     println!(r"cargo:rustc-link-lib=dylib=hm");
-    println!(r"cargo:rustc-link-search=native=./build");
+    println!(r"cargo:rustc-link-search=native={}", &out_dir);
 }
