61c8c95d01f7855b227c7e5fce5a952eb0a82165
Jun Kimura <junkxdev@gmail.com>
2019-09-09 00:29:00 +0900

    remove src dir

--- src/lib.rs
+++
@@ -1,5 +0,0 @@
-// use hmcdk::error;
-// use hmcdk::prelude::*;
-// use std::cell::RefCell;
-// use std::collections::HashMap;
-// use std::panic::{catch_unwind, resume_unwind, AssertUnwindSafe, UnwindSafe};
